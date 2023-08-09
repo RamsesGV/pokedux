d39e6a023d99fa4885dc7baad06f2d0db575bf27
Ramses Garay <senkustone15@gmail.com>
2023-08-09 10:32:31 -0600

    redux conection

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
-import './App.css'
-import { Col } from 'antd'
-import Searcher from './components/Searcher/Searcher'
-import PokemonList from './components/PokemonList/PokemonList'
-import logo from './statics/logo.svg'
-import { useEffect, useState } from 'react'
-import { getPokemon } from './api'
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Col } from 'antd';
+import Searcher from './components/Searcher/Searcher';
+import PokemonList from './components/PokemonList/PokemonList';
+import { getPokemon } from './api';
+import { setPokemons as setPokemonsActions } from './actions';
+import logo from './statics/logo.svg';
+import './App.css';
 
-
-
-function App() {
-const [pokemons, setPokemons] = useState([])
-
-  useEffect(() => { 
-    const fetchPokemons = async () =>  {
-      const pokemonsRes = await getPokemon()
-      setPokemons(pokemonsRes)
+function App({ pokemons, setPokemons }) {
+  console.log("🚀 ~ file: App.js ~ line 12 ~ App ~ pokemons", pokemons)
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const pokemonsRes = await getPokemon();
+      setPokemons(pokemonsRes);
     };
-      fetchPokemons()
-  },[])
+
+    fetchPokemons();
+  }, []);
 
   return (
-    <>
-    <div className='App'> 
+    <div className='App'>
       <Col span={4} offset={10}>
-      <img src={logo} alt='pokedux' />
+        <img src={logo} alt='Pokedux' />
       </Col>
-      <Col span={8} offset={8}> 
-        <Searcher/>
+      <Col span={8} offset={8}>
+        <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons}/>
+      <PokemonList pokemons={pokemons} />
     </div>
-  
-    </>
-  )
+  );
 }
 
-export default App
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
